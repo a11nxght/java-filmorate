67948eeec6c5bbb37c68d17783e1d23666dcfca1
a11nxght <a11nxght@yandex.kz>
2025-04-03 09:15:30 +0300

    Создать модели данных

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,19 +2,15 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NonNull;
 
-import java.time.Duration;
 import java.time.LocalDate;
-
 
 @Data
 @AllArgsConstructor
-public class Film {
+public class User {
     private Long id;
-    @NonNull
+    private String email;
+    private String login;
     private String name;
-    private String description;
-    private LocalDate releaseDate;
-    private Duration duration;
+    private LocalDate birthday;
 }
